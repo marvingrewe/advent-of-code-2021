4da0a13ec06aec5de7006941dd70ef183f5218fd
Marvin Grewe <marvin@grewe.org>
2021-12-01 21:01:36 +0100

    adds Day01 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,26 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var depthIncreases = 0
+        for (i in 1 until input.size) {
+            if (input[i].toInt() > input[i - 1].toInt()) depthIncreases++
+        }
+        return depthIncreases
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var depthIncreases = 0
+        var previousWindow = input[0].toInt() + input[1].toInt() + input[2].toInt()
+        for (i in 3 until input.size) {
+            val currentWindow = input[i].toInt() + input[i - 1].toInt() + input[i - 2].toInt()
+            if (currentWindow > previousWindow) depthIncreases++
+            previousWindow = currentWindow
+        }
+        return depthIncreases
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
