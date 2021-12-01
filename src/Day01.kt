3fd0ed220daf7e7fd86b857f34f826d8aa33c9aa
Marvin Grewe <marvin@grewe.org>
2021-12-01 21:30:05 +0100

    adds Day01 functional solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -18,6 +18,11 @@ fun main() {
         return depthIncreases
     }
 
+    fun part2func(rawInput: List<String>): Int {
+        val input = rawInput.map(String::toInt)
+        return input.windowed(3, 1).map { it.sum() }.zipWithNext().count { it.first < it.second }
+    }
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part2(testInput) == 5)
@@ -25,4 +30,5 @@ fun main() {
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+    println(part2func(input))
 }
