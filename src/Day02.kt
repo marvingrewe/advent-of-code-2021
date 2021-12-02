85ba68953aa7fdca74229a1f4aa84cc4ce0f39f3
Marvin Grewe <marvin@grewe.org>
2021-12-02 12:20:07 +0100

    add Day02 solution

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,11 +1,29 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        return 0
-    }
+import java.lang.IllegalArgumentException
 
-    fun part2(input: List<String>): Int {
-        return 0
-    }
+fun main() {
+    fun part1(input: List<String>): Int = input.fold(Pair(0, 0)) { (depth, distance), element ->
+        val (direction, value) = element.split(" ")
+        when (direction) {
+            "forward" -> Pair(depth, distance + value.toInt())
+            "down" -> Pair(depth + value.toInt(), distance)
+            "up" -> Pair(depth - value.toInt(), distance)
+            else -> throw IllegalArgumentException()
+        }
+    }.let { it.first * it.second }
+
+    fun part2(input: List<String>): Int = input.fold(Triple(0, 0, 0)) { (depth, distance, aim), element ->
+        val (direction, value) = element.split(" ")
+        when (direction) {
+            "forward" -> Triple(depth + aim * value.toInt(), distance + value.toInt(), aim)
+            "down" -> Triple(depth, distance, aim + value.toInt())
+            "up" -> Triple(depth, distance, aim - value.toInt())
+            else -> throw IllegalArgumentException()
+        }
+    }.let { it.first * it.second }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02_test")
+    check(part2(testInput) == 900)
 
     val input = readInput("Day02")
     println(part1(input))
