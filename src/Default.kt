0efb9b55cab47ff90efd7101bd86d50d476865a6
Marvin Grewe <marvin@grewe.org>
2021-12-13 00:39:30 +0100

    update scaffolding

--- src/Default.kt
+++ src/Default.kt
@@ -17,6 +17,8 @@ fun main() {
     val input = readInput("Day")
     var result: Any
 
+    println("Test 1 solved in ${measureTimeMillis { result = part1(testInput) }}ms with result: $result, expected: ")
+    println("Test 2 solved in ${measureTimeMillis { result = part2(testInput) }}ms with result: $result, expected: ")
     println("Part 1 solved in ${measureTimeMillis { result = part1(input) }}ms with result: $result")
     println("Part 2 solved in ${measureTimeMillis { result = part2(input) }}ms with result: $result")
 }
